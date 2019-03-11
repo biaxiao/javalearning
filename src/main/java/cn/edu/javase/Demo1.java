938abc03b3bee47dac65641a6eaf784ace37c577
biaxiao <953692809@qq.com>
2019-03-11 11:34:06 +0800

    intern String 1.6 1.7

---
+++ src/main/java/cn/edu/javase/Demo1.java
@@ -0,0 +1,20 @@
+package cn.edu.javase;
+
+public class Demo1 {
+//intern 1.6 1.7
+    public static void main(String[] args) {
+        String s = new String("1");
+        System.out.println(s.intern());
+        //System.out.println(s==s.intern());
+        String s2 = "1";
+        //System.out.println("ss:"+(s.intern() == s2));
+        System.out.println(s == s2);
+
+        String s3 = new String("1") + new String("1");
+        s3.intern();
+        String s4 = "11";
+        System.out.println(s3 == s4);
+    }
+
+
+}
