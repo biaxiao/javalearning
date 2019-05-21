8830b2b241ad616ec413916579c68fd16b216acc
biaxiao <953692809@qq.com>
2019-05-21 22:06:07 +0800

    三种遍历，循环和递归实现

--- src/main/java/offer/树/Demo7.java
+++ src/main/java/offer/树/Demo7.java
@@ -1,4 +1,9 @@
 package offer.树;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * 已知二叉树的前序遍历和中序遍历，重建二叉树
  * 2019年5月19日
@@ -12,7 +17,19 @@ public class Demo7 {
 		System.out.println(construct);
 		//先序遍历
 		preTraverseBinTree(construct);
+		System.out.println();
+		preTraverseBinTree2(construct);
+		System.out.println("\n----------------");
+		postTraverseBinTree(construct);
+		System.out.println();
+		postTraverseBinTree2(construct);
+		System.out.println("\n----------------");
+		inTraverseBinTree(construct);
+		System.out.println();
+		inTraverseBinTree2(construct);
 	}
+	
+	//返回头结点
 	static BinaryTreeNode Construct(int[] preOrder,int[] inOrder) {
 		return ConstructCore(preOrder, 0, preOrder.length-1, inOrder, 0, inOrder.length-1);
 	}
@@ -33,45 +50,111 @@ public class Demo7 {
 		return rootNode;
 	}
 
-	// 将二叉树先序遍历，用于测试结果
-	public static void preTraverseBinTree(BinaryTreeNode node) {
-		if (node == null) {
+	// 将二叉树前序遍历，用于测试结果
+	public static void preTraverseBinTree(BinaryTreeNode root) {
+		if (root == null) {
 			return;
 		}
-		System.out.print(node.value + " ");
-		if (node.pleft != null) {
-			preTraverseBinTree(node.pleft);
+		System.out.print(root.value + " ");
+		if (root.pleft != null) {
+			preTraverseBinTree(root.pleft);
 		}
-		if (node.pright != null) {
-			preTraverseBinTree(node.pright);
+		if (root.pright != null) {
+			preTraverseBinTree(root.pright);
 		}
 	}
 
+	public static void preTraverseBinTree2(BinaryTreeNode root) {
+		if(root==null) {
+			return;
+		}
+		Stack<BinaryTreeNode> s=new Stack<>();
+		s.push(root);
+		while(!s.empty()) {
+			BinaryTreeNode e = s.pop();
+			System.out.print(e.value+" ");
+			if(e.pright!=null) {
+				s.push(e.pright);
+			}
+			if(e.pleft!=null) {
+				s.push(e.pleft);
+			}
+		}
+		
+		
+	}
+	
 	// 将二叉树中序遍历，用于测试结果
-	public static void inTraverseBinTree(BinaryTreeNode node) {
-		if (node == null) {
+	public static void inTraverseBinTree(BinaryTreeNode root) {
+		if (root == null) {
 			return;
 		}
-		if (node.pleft != null) {
-			inTraverseBinTree(node.pleft);
+		if (root.pleft != null) {
+			inTraverseBinTree(root.pleft);
 		}
-		System.out.print(node.value + " ");
-		if (node.pright != null) {
-			inTraverseBinTree(node.pright);
+		System.out.print(root.value + " ");
+		if (root.pright != null) {
+			inTraverseBinTree(root.pright);
 		}
 	}
 
-	// 将二叉树后序遍历，用于测试结果
-	public static void postTraverseBinTree(BinaryTreeNode node) {
-		if (node == null) {
+	public static void inTraverseBinTree2(BinaryTreeNode root) {
+		if (root == null) {
 			return;
 		}
-		if (node.pleft != null) {
-			postTraverseBinTree(node.pleft);
-		}
-		if (node.pright != null) {
-			postTraverseBinTree(node.pright);
-		}
-		System.out.print(node.value + " ");
+		
+		Stack<BinaryTreeNode> s=new Stack<BinaryTreeNode>();
+		
+		do {
+			while(root!=null) {
+				s.push(root);
+				root=root.pleft;
+			}
+			if(!s.empty()) {
+				BinaryTreeNode e = s.pop();
+				System.out.print(e.value+" ");
+				root=e.pright;
+			}
+		}while(!s.empty()||root!=null);
+		
 	}
+	
+	
+	// 将二叉树后序遍历，用于测试结果
+	public static void postTraverseBinTree(BinaryTreeNode root) {
+		if (root == null) {
+			return;
+		}
+		if (root.pleft != null) {
+			postTraverseBinTree(root.pleft);
+		}
+		if (root.pright != null) {
+			postTraverseBinTree(root.pright);
+		}
+		System.out.print(root.value + " ");
+	}
+	
+	public static void postTraverseBinTree2(BinaryTreeNode root) {
+		if (root == null) {
+			return;
+		}
+		Stack<BinaryTreeNode> s=new Stack<>();
+		List<Integer> list=new ArrayList<Integer>();
+		s.push(root);
+		while(!s.empty()) {
+			BinaryTreeNode e = s.pop();
+			if(e.pleft!=null) {
+				s.push(e.pleft);
+			}
+			if(e.pright!=null) {
+				s.push(e.pright);
+			}
+			list.add(0, e.value);
+		}
+		
+		for (Integer integer : list) {
+			System.out.print(integer+" ");
+		}
+	}
+	
 }
